79e0194a4bea9bda3c3b9ed36d149ac1a079ae84
James-Cochran <jameslikesfruit@gmail.com>
2024-10-09 15:46:49 -0400

    added selector for random poster button, added event listener for random poster button, tied it to the pageload existing function.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 let image = document.querySelector('.poster-img')
 let title = document.querySelector('.poster-title')
 let quote = document.querySelector('.poster-quote')
+let showRandomButton = document.querySelector('.show-random')
 
 
 
@@ -110,7 +111,7 @@ var currentPoster;
 
 // event listeners go here 👇
 window.onload = pageLoad
-
+showRandomButton.addEventListener('click', pageLoad)
 // functions and event handlers go here 👇
 // (we've provided two to get you started)!
 function getRandomIndex(array) {
